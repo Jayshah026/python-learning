1faff816f3e67f8bb2a814b7cad1a99a0e4eb159
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-08 10:05:32 +0530

    Add script to print random jokes using pyjokes

--- Basics/pipEx.py
+++ Basics/pipEx.py
@@ -14,6 +14,10 @@ Why do we want intelligent terminals when there are so many stupid users?
 3 Database Admins walked into a NoSQL bar. A little while later they walked out because they couldn't find a table.
 
 
+"""
+
 Explanation : 
 
-"Here we have imported pyjokes and when we run it we get 1 joke. We can run this as many times as we want to get a joke from it." 
+Here we have imported pyjokes and when we run it we get 1 joke. We can run this as many times as we want to get a joke from it. 
+
+""" 
