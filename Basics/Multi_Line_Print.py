fa176303286c18d04bf79c69f79a2a78284056d4
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-08 10:32:19 +0530

    Create Multi_Line_Print.py

---
+++ Basics/Multi_Line_Print.py
@@ -0,0 +1,24 @@
+Code : 
+
+print('''Code is fun, code is bright,
+Learning Python day and night.
+Errors come, but don’t you fear,
+Solutions soon will all appear.
+''')
+
+
+OUTPUT : 
+
+Code is fun, code is bright,
+Learning Python day and night.
+Errors come, but don’t you fear,
+Solutions soon will all appear.
+
+
+""" 
+
+Explanation : 
+
+Here we can write and print multiple lines by using... print(''' Code ''') OR print(""" Code """)
+
+"""
