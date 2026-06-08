d8e03e8bb958be9b13137c21139817710ec958ba
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-08 09:55:10 +0530

    Add script to print random jokes using pyjokes
    This script imports the pyjokes library and prints a random joke each time it's run.

---
+++ Basics/pipEx.py
@@ -0,0 +1,19 @@
+Code : 
+
+import pyjokes
+
+joke = pyjokes.get_joke()
+
+print(joke)
+
+
+OUTPUT : 
+
+Why do we want intelligent terminals when there are so many stupid users?
+
+3 Database Admins walked into a NoSQL bar. A little while later they walked out because they couldn't find a table.
+
+
+Explanation : 
+
+"Here we have imported pyjokes and when we run it we get 1 joke. We can run this as many times as we want to get a joke from it." 
