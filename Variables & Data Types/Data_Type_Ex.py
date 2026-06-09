a63f42bfa64ef00f01b089c949f7391242bd0521
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-09 14:29:52 +0530

    Add Python script to demonstrate data types
    This script demonstrates various data types in Python, including integer, float, string, boolean, and None. It prints each variable along with its corresponding data type description.

---
+++ Variables & Data Types/Data_Type_Ex.py
@@ -0,0 +1,22 @@
+Code : 
+
+a = 1     # Interger data type
+b = 1.2   # Decimal value (Float type)
+c = "Jay" # String data type
+d = True  # Boolean data type
+e = None  # None data type 
+
+print(a, " - It's a Integer data type")
+print(b, " - It's a Float data type")
+print(c, " - It's a String data type")
+print(d, " - It's a Boolean data type")
+print(e, " - It's a None data type")
+
+
+OUTPUT : 
+
+1  - It's a Integer data type
+1.2  - It's a Float data type
+Jay  - It's a String data type
+True  - It's a Boolean data type
+None  - It's a None data type
