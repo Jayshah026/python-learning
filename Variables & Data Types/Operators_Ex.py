2321dfa0d29c3ea731e5989ab2370b32dad87118
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-09 14:44:23 +0530

    Add Operators_Ex.py program to demonstrate Python operators
    This file demonstrates various operators in Python, including arithmetic, assignment, conditional, and logical operators with example outputs.

---
+++ Variables & Data Types/Operators_Ex.py
@@ -0,0 +1,74 @@
+Code : 
+
+a = 10
+b = 2
+
+print("Arithmatic Operator")
+print (a + b)
+print (a - b)
+print (a * b)
+print (a / b)
+print (a % b)
+print("\n")
+
+
+print("Assignment Operator")
+b = a 
+print(b)
+b = 2
+
+a += a
+print(a)
+a = 10
+
+a -= b
+print(a)
+a = 10
+print("\n")
+
+
+print("Conditional Operator")
+print(a == b)
+print(a > b)
+print(a >= b)
+print(a < b)
+print(a <= b)
+print(a != b)
+print("\n")
+
+
+print("Logical Operator")
+print(a and b == 3)
+print(a or b == 10)
+print(not(a))
+
+
+OUTPUT : 
+
+Arithmatic Operator
+12
+8
+20
+5.0
+0
+
+
+Assignment Operator
+10
+20
+8
+
+
+Conditional Operator
+False
+True
+True
+False
+False
+True
+
+
+Logical Operator
+False
+10
+False
