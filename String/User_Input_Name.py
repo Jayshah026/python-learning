b076172b5b51632314555fa58712f4311cc6ef55
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-10 13:00:50 +0530

    Add user input greeting script
    This script prompts the user to enter a name and greets them using both a simple print method and an f-string method.

---
+++ String/User_Input_Name.py
@@ -0,0 +1,14 @@
+Code : 
+
+name = input("Enter a name : ")
+
+print("Good Afternoon ", name) # Using simple method
+
+print(f"Good afternoon {name}") # Using f string method
+
+
+OUTPUT : 
+
+Enter a name : Jay
+Good Afternoon  Jay
+Good afternoon Jay
