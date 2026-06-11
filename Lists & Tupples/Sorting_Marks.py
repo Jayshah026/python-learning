35fa5ef67c8f4b5aa428648b2d2fad91f1da180d
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-11 11:01:21 +0530

    Add script to collect and sort student marks
    This script collects marks from six students, stores them in a list, and sorts the list in ascending order before printing it.

---
+++ Lists & Tupples/Sorting_Marks.py
@@ -0,0 +1,37 @@
+Code : 
+
+Marks = []
+
+Student1 = input("Enter the marks : ")
+Marks.append(Student1)
+
+Student2 = input("Enter the marks : ")
+Marks.append(Student2)
+
+Student3 = input("Enter the marks : ")
+Marks.append(Student3)
+
+Student4 = input("Enter the marks : ")
+Marks.append(Student4)
+
+Student5 = input("Enter the marks : ")
+Marks.append(Student5)
+
+Student6 = input("Enter the marks : ")
+Marks.append(Student6)
+
+print(Marks)
+Marks.sort()
+print(Marks)
+
+
+OUTPUT : 
+
+Enter the marks : 55
+Enter the marks : 36
+Enter the marks : 98
+Enter the marks : 41
+Enter the marks : 73
+Enter the marks : 25
+['55', '36', '98', '41', '73', '25']
+['25', '36', '41', '55', '73', '98']
