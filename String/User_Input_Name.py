53bca31780bc7f3110cb9fe55b594f0f8f386f05
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-19 10:52:27 +0530

    Enhance documentation on string input methods
    Added explanation for string formatting methods in Python.

--- String/User_Input_Name.py
+++ String/User_Input_Name.py
@@ -12,3 +12,11 @@ OUTPUT :
 Enter a name : Jay
 Good Afternoon  Jay
 Good afternoon Jay
+
+
+""" 
+- Explanation : 
+
+Here in 1st string we have used a normal method which is old. Here First we write the sentence in " " and after that we called the varialbe using " , ".
+
+In f string first we use " f " into the string and then start with " Sentence  {Variable Call} ". Here like this we can directly call variables. 
