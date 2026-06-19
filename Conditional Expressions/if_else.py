facc7557e21a2829223aebb3a30adff9df17f832
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-19 15:22:24 +0530

    Implement age eligibility check for voting
    Add a simple age check for voting eligibility.

---
+++ Conditional Expressions/if_else.py
@@ -0,0 +1,15 @@
+Code : 
+
+a = int(input("Enter your age : "))
+
+if(a >= 18):
+    print("You are eligible for voting")
+
+else:
+    print("You are miner")
+
+
+OUTPUT : 
+
+Enter your age : 17
+You are minor
