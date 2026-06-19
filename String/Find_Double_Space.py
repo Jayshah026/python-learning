da6108a138847965eb96c1165f38ab68022832dc
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-19 10:45:52 +0530

    Create Program for string analysis

--- String/Find_Double_Space.py
+++ String/Find_Double_Space.py
@@ -10,6 +10,6 @@ print(String.find("   "))
 
 OUTPUT : 
 
-14    # Here it's an index showing where the element is that we are finding. Here double space detected on 11th index.
+14    # Here it's an index showing where the element is that we are finding. Here double space detected on 1th index.
 11    # Here "J" is detected on 11th Index
 -1    # Here there is no "   " (Three spaces) so here the value is -1 showing that element we are finding is not exits. 
