fb443f043b8a1dc94d25333a614c5944265aa5b3
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-20 17:05:36 +0530

    Adding for loop example with the use of else

---
+++ 7. Loops/for_loop_with_else.py
@@ -0,0 +1,20 @@
+L1 = [1, 23, 46, 69]
+
+for i in L1:
+    print(i)
+
+else:  # After finishing printing the values in the L1(List) the else statement will executes
+    print("Done printing values")
+
+
+
+
+"""
+OUTPUT 
+
+1
+23
+46
+69
+Done printing values
+"""
