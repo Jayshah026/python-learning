8ac3cb650acb60ddfa7c8593f4d7d5d547ad9057
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-20 21:31:23 +0530

    Adding example to show use of startswith method

---
+++ 7. Loops/greeting_message_print.py
@@ -0,0 +1,16 @@
+List = ["Jay", "Jenny", "Rekha", "Karan", "Jiya"]
+
+for name in List:
+    if(name.startswith("J")):
+        print(f"Hello {name}")
+
+
+
+
+"""
+OUTPUT : 
+
+Hello Jay
+Hello Jenny
+Hello Jiya
+"""
