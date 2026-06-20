205336ca5d02e00a4a1573533d107e51e9958fa2
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-20 17:33:31 +0530

    Adding example using pass statement

---
+++ 7. Loops/loop_with_pass_Ex.py
@@ -0,0 +1,18 @@
+for i in range(1, 501):
+    pass # By this we can hold this loop to being processed and can process further with different activity
+
+
+for i in range(1, 5):
+    print(i)
+
+
+
+
+"""
+OUTPUT : 
+
+1
+2
+3
+4
+"""
