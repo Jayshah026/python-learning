7b6e543237784c108d994ac1c6e27c707d84d31b
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-22 17:05:41 +0530

    Adding multiplication table in both correct and reverse order

--- 7. Loops/multiplication_table_Ex.py
+++ 7. Loops/multiplication_table_Ex.py
@@ -5,9 +5,18 @@ for i in range(1, 11):
 
 
 
+n = int(input("Enter a number for multiplication table for reverse order : "))
+
+for i in range(10, 0, -1):
+    print(f"{n} x {i} = {n * i}")
+
+
+
+
 """
 OUTPUT : 
 
+Enter a number for multiplication table : 10
 10 x 1 = 10
 10 x 2 = 20
 10 x 3 = 30
@@ -18,4 +27,16 @@ OUTPUT :
 10 x 8 = 80
 10 x 9 = 90
 10 x 10 = 100
+Enter a number for multiplication table for reverse order : 10
+10 x 10 = 100
+10 x 9 = 90
+10 x 8 = 80
+10 x 7 = 70
+10 x 6 = 60
+10 x 5 = 50
+10 x 4 = 40
+10 x 3 = 30
+10 x 2 = 20
+10 x 1 = 10
+
 """
