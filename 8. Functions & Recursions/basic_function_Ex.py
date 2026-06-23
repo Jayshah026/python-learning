b93d35a6917c45f5f54c65bea299451b681e485c
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-23 12:42:12 +0530

    Adding function to greet a user

---
+++ 8. Functions & Recursions/basic_function_Ex.py
@@ -0,0 +1,21 @@
+def greetings():
+    name = input("Enter your name : ")
+    print(f"Have a good day {name}")
+    print("")
+
+
+greetings()
+greetings()
+
+
+
+"""
+OUTPUT : 
+
+Enter your name : Jay
+Have a good day Jay
+
+Enter your name : Heet
+Have a good day Heet
+
+"""
