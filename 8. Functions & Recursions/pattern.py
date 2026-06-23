abfeec054a8990ba4eff32571b9f1f74808321b8
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-23 15:23:06 +0530

    Printing inverted right‑angled triangle patter using recursion

---
+++ 8. Functions & Recursions/pattern.py
@@ -0,0 +1,19 @@
+"""
+To print this pattern :
+
+*****
+****
+***
+**
+*
+
+"""
+
+def pattern(n):
+    if(n == 0):
+        return
+    else:
+        print("*" * n)
+        pattern(n - 1)
+
+pattern(5)
