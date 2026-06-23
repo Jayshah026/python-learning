ab86c6a5a32de253e60d815e4f8a59653071adc4
Jayshah026 <jayshah01.jds@gmail.com>
2026-06-23 14:08:13 +0530

    Adding example of function argument

---
+++ 8. Functions & Recursions/function_with_argument.py
@@ -0,0 +1,21 @@
+def greeting(name, ending):
+    print("Hello " + name)
+    print(ending)
+    print("")
+
+greeting("Jay", "Thank you")
+greeting("Diya", "Thanks")
+
+
+
+
+"""
+OUTPUT : 
+
+Hello Jay
+Thank you
+
+Hello Diya
+Thanks
+
+"""
